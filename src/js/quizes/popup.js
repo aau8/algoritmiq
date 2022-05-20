bd22c7a2264e4f844fc55f419a5ebf2d1bc36b0f
Artem <artiom.ygryumov@gmail.com>
2022-05-20 17:50:46 +0300

    вставил квиз на страницу

--- src/js/quizes/popup.js
+++ src/js/quizes/popup.js
@@ -15,34 +15,34 @@ if (popupOffer) {
         popupOffer.classList.remove('_show')
     })
     
-    btnStart.addEventListener('click', async e => {
-        const modal = document.getElementById('pquiz')
-        const modalWindow = modal.querySelector('.modal__window')
-        const slider = modal.querySelector('.quiz-slider')
-        const sliderWrapper = slider.querySelector('.swiper-wrapper')
+    // btnStart.addEventListener('click', async e => {
+    //     const modal = document.getElementById('pquiz')
+    //     const modalWindow = modal.querySelector('.modal__window')
+    //     const slider = modal.querySelector('.quiz-slider')
+    //     const sliderWrapper = slider.querySelector('.swiper-wrapper')
 
-        if (sliderWrapper.innerHTML.trim() == '') {
-            await fetch('./pop-up-quiz-slides.html')
-            .then(data => data.text())
-            .then(html => {
-                sliderWrapper.innerHTML = html
-            })
-            .then(e => {
-                const quizForm = modal.querySelector('.quiz-modal-form')
-                const qfInputElems = quizForm.querySelectorAll('.textfield input')
-                const rangeBlockElems = sliderWrapper.querySelectorAll('.qmca-range')
+    //     if (sliderWrapper.innerHTML.trim() == '') {
+    //         await fetch('./pop-up-quiz-slides.html')
+    //         .then(data => data.text())
+    //         .then(html => {
+    //             sliderWrapper.innerHTML = html
+    //         })
+    //         .then(e => {
+    //             const quizForm = modal.querySelector('.quiz-modal-form')
+    //             const qfInputElems = quizForm.querySelectorAll('.textfield input')
+    //             const rangeBlockElems = sliderWrapper.querySelectorAll('.qmca-range')
 
-                quantSlides(quizSlider)
-                initRanges(rangeBlockElems)
-                initForm(quizForm)
+    //             quantSlides(quizSlider)
+    //             initRanges(rangeBlockElems)
+    //             initForm(quizForm)
 
-                qfInputElems.forEach(initLabelTextfield)
+    //             qfInputElems.forEach(initLabelTextfield)
 
-                modalWindow.classList.remove('_preload')
-                slider.classList.remove('_hide')
-            })
-        }
-    })
+    //             modalWindow.classList.remove('_preload')
+    //             slider.classList.remove('_hide')
+    //         })
+    //     }
+    // })
     
     setTimeout(e => {
         popupOffer.classList.add('_show')
